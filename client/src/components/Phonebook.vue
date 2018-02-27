2dc904972a0a6becbac2b6d88ce82cced3cf82f2
steve <bashcandicoot@gmail.com>
2018-02-27 19:33:12 +0000

    added sorting/searching, address on click, and some css

--- client/src/components/Phonebook.vue
+++ client/src/components/Phonebook.vue
@@ -1,9 +1,16 @@
 <template>
-  <div class="hello">
+  <div>
     <h1>{{ header }}</h1>
-    <div v-for="contact in contacts" v-bind:key="contact">
-      <div>
-        {{ contact.name }} <span>{{ contact.phone_number }}</span>
+    <input type="text" name="search" v-model="search" placeholder="Search name/number..." class='search'>
+    <button class="sort-button" @click="sortContactsByLastName">SORT</button>
+    <div class="border"></div>
+    <div class="contact" v-for="contact in filteredContacts" v-bind:key="contact.phone_number">
+      <div @click="showAddress(contact)">
+        <div class="name">{{ contact.name }}</div>
+        <div class="phone-number">{{ contact.phone_number }}</div>
+        <div class="address" v-if="selected === contact.phone_number">
+          {{ contact.address }}
+        </div>
       </div>
     </div>
   </div>
@@ -17,42 +24,115 @@ export default {
   data() {
     return {
       header: 'Phonebook',
-      contacts: null
+      contacts: null,
+      selected: null,
+      search: null,
+      sortDirection: '<'
     };
   },
   created() {
     this.getContacts()
+  },
+  computed: {
+    filteredContacts() {
+      if (!this.search) {
+        return this.contacts;
+      }
+      return this.filterContactsByName();
+    },
   },
   methods:{
     getContacts() {
       axios.get('http://www.mocky.io/v2/581335f71000004204abaf83')
       .then(response => {
         this.contacts = response.data.contacts;
-        // this.createPhonebook(this.contacts)
       })
       .catch(error => {
         console.error(error);
       })
     },
-
+    showAddress(contact) {
+      this.selected = contact.phone_number;
+    },
+    filterContactsByName() {
+      return this.contacts.filter((contact) => {
+        const name = contact.name.toLowerCase();
+        const number = contact.phone_number;
+        const search = this.search.toLowerCase();
+        return name.includes(search) || number.includes(search);
+      });
+    },
+    sortContactsByLastName() {
+      if (this.sortDirection === '<') {
+        this.contacts.sort((a, b) => a.name.split(' ')[1] > b.name.split(' ')[1]);
+        this.sortDirection = '>';
+      } else {
+        this.contacts.sort((a, b) => a.name.split(' ')[1] < b.name.split(' ')[1]);
+        this.sortDirection = '<';
+      }
+    }
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+
+h1 {
+  font-weight: bold;
+  color: #bd8564;
+  height: 100px;
+  padding: 30px 20px 20px 20px;
+  background-color: #50392c;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.border {
+  border-bottom: 3px double #8c8b8b;
+  padding: 2em;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.sort-button {
+  border: none;
+  display: block;
+  text-align: center;
+  cursor: pointer;
+  position: relative;
+  color: #bd8564;
+  font-weight: 700;
+  font-size: 15px;
+  background-color: #50392c;
+  padding: 9px 60px;
+  margin: 0 auto;
+  box-shadow: 0 5px 15px rgba(0,0,0,0.20);
 }
-a {
-  color: #42b983;
+
+.search {
+  text-align: center;
+  padding: 20px 20px 20px 20px;
+  font-size: 20px;
 }
+
+.contact {
+  padding: 2em;
+  cursor: pointer;
+  border-bottom: 3px double #8c8b8b;
+}
+
+.contact:hover {
+  color: #bd8564;
+}
+
+.name {
+  font-size: 20px;
+}
+
+.phone-number {
+  opacity: 0.6;
+}
+
+.address {
+  opacity: 0.6;
+  font-size: 14px;
+}
+
 </style>
